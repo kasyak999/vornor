ea422ef7fb2ff50c6b4297f5da82b0320f7020b7
Максим <kasyak999@yandex.ru>
2025-10-06 21:38:07 +0300

    up

---
+++ src/app/core/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Настройки подключения к базе данных."""
+
+    postgres_user: str = 'user'
+    postgres_password: str = 'mysecretpassword'
+    postgres_db: str = 'django'
+    postgres_port: str = '5433'
+    secret: str = 'SECRET'
+    DEBUG: bool = False
+
+    @property
+    def database_url(self) -> str:
+        """Формирует URL подключения к базе."""
+        db = (
+            'postgresql+asyncpg://'
+            f'{self.postgres_user}:{self.postgres_password}@'
+        )
+        db_host = f'localhost:{self.postgres_port}' if self.DEBUG else 'db'
+        return db + db_host + f'/{self.postgres_db}'
+
+    class Config:
+        env_file = './.env'
+
+
+settings = Settings()
---
+++ src/app/core/db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from sqlalchemy.orm import declarative_base, sessionmaker, declared_attr
+from app.core.config import settings
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+
+class PreBase:
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=PreBase)
+engine = create_async_engine(settings.database_url)
+# AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session():
+    async with AsyncSessionLocal() as async_session:
+        yield async_session
