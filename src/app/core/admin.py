3f957feab07daed1f685f3329882bd94b0ca9959
Максим <kasyak999@yandex.ru>
2025-11-14 22:33:05 +0300

    up

--- src/app/core/admin.py
+++ src/app/core/admin.py
@@ -33,10 +33,12 @@ class UserAdmin(ModelView, model=User):
 
 class CoinAdmin(ModelView, model=Coin):
     column_list = [
-        Coin.name, Coin.user_id, Coin.cycle, Coin.start, Coin.created_at]
+        Coin.name, Coin.user, Coin.cycle, Coin.start, Coin.created_at]
     name = "монету"
     name_plural = "Монеты"
-    column_labels = get_column_comments(Coin)
+    column_labels = {**get_column_comments(User), **{
+        "user": "Телеграм id",
+    }}
 
 
 class CoinSlotAdmin(ModelView, model=CoinSlot):
